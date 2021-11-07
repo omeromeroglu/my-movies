81b0c2bcd479cdea3a69b9a37bd28b7b16c7d1be
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-11-07 23:59:09 +0300

    my-movies

---
+++ src/components/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import axios from 'axios';
+
+class App extends React.Component {
+
+    state = {
+        movies: [],
+
+        searchQuery: ""
+    }
+
+  /*   async componentDidMount(){
+        const baseUrl = "http://localhost:3002/movies";
+        const response = await fetch(baseUrl) ;
+        const data= await response.json();
+        this.setState({movies :data})
+    } */
+
+    async componentDidMount(){
+        const response = await axios.get("http://localhost:3002/movies");
+        this.setState({movies: response.data})
+    }
+
+  /*   deleteMovie = (movie) => {
+        const newMovieList = this.state.movies.filter(
+            m => m.id !== movie.id
+        ); */
+
+     deleteMovie = async (movie) => {
+
+            axios.delete(`http://localhost:3002/movies/${movie.id}`)
+            const newMovieList = this.state.movies.filter(
+                m => m.id !== movie.id
+            );
+
+/*         this.setState ({
+            movies: newMovieList
+        }) */
+
+        this.setState(state => ({
+            movies: newMovieList
+        }))
+    }
+
+    searchMovie = (event) => {
+        //console.log(event.target.value)
+        this.setState({searchQuery: event.target.value })
+    }
+
+    render() {
+
+        let filteredMovies = this.state.movies.filter(
+            (movie) => {
+                return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+            }
+        )
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-12">
+                        <SearchBar searchMovieProp={this.searchMovie} />
+                    </div>
+                </div>
+
+                <MovieList
+                    movies={filteredMovies}
+                    deleteMovieProp={this.deleteMovie} />
+            </div>
+        )
+
+    }
+
+
+}
+
+export default App;
